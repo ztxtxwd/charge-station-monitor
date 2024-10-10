10e3dedd13d78ce183cd843542bd5c15d740eb27
ztxtxwd <ztx4215@gmail.com>
2024-10-10 18:32:44 +0000

    bug fix

--- server/api/stations/index.post.ts
+++ server/api/stations/index.post.ts
@@ -1,13 +1,14 @@
 import { useValidatedBody, z } from 'h3-zod'
 
 export default eventHandler(async (event) => {
-  const { code } = await useValidatedBody(event, {
-    code: z.string().min(1).max(100)
+  const { stationCode } = await useValidatedBody(event, {
+    stationCode: z.string().min(1).max(100)
   })
 
   // Insert station for the current user
   const station = await useDB().insert(tables.stations).values({
-    code: code,
+    stationCode,
+    createdAt: new Date()
   }).returning().get()
 
   return station
